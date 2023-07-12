522c9c65f37d3301a4ad4e0fcbf65cc6077f33b8
Vitaliy Zayarniy <ultwolf@gmail.com>
2023-07-12 20:34:01 +0300

    Add project files.

---
+++ Generator/HelloSourceGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Generator
+{
+
+    [Generator]
+    public sealed class HelloSourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var compilationIncrementalValue = context.CompilationProvider;
+
+            context.RegisterSourceOutput(
+                compilationIncrementalValue,
+                static (context, compilation) =>
+                {
+                    // Get the entry point method
+                    var mainMethod = compilation.GetEntryPoint(context.CancellationToken);
+                    var typeName = mainMethod.ContainingType.Name;
+
+                    string source = $$"""
+                // Auto-generated code
+                namespace {{mainMethod.ContainingNamespace.ToDisplayString()}};
+                
+                public static partial class {{typeName}}
+                {
+                    static partial void HelloFrom(string name) =>
+                        Console.WriteLine($"Generator says: Hi from '{name}'");
+                }                
+                """;
+
+                    // Add the source code to the compilation
+                    context.AddSource($"{typeName}.g.cs", source);
+                });
+        }
+    }
+}
